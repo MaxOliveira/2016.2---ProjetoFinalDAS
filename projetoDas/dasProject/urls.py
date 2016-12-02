ef1c520fdb21655105d701214ea4736555a34547
Eduardo Moreira <moreira.edu@hotmail.com>
2016-12-01 23:42:12 -0200

    Added face detection

--- projetoDas/dasProject/urls.py
+++ projetoDas/dasProject/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/', views.index, name=''),
+    url(r'^', views.index, name='index'),
+    url(r'^faceDetection/', views.faceDetection, name='faceDetection'),
     url(r'^load_files/', views.process_files),
     url(r'^load_folder/', views.process_folder)
 ]
